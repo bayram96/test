e6ccde1988f83b2f4c6d2ab4c5ab5577a67e1197
osx <osx@osxs-MacBook-Air.local>
2017-03-03 16:24:25 -0500

    added ne files

---
+++ ruby/garalama.rb
@@ -0,0 +1,12 @@
+puts 'Adyn name'
+name = gets.chomp
+puts "salam " + name + " Kopeltjek sanlaryny birinjisini giriz  please"
+
+san1 =gets.chomp
+puts "ikinjisini giriz"
+san2 =gets.chomp
+def kopelt(num1,num2)
+   puts num1.to_i*num2.to_i	 
+end	
+puts name + " netije shul"
+kopelt(san1,san2)
